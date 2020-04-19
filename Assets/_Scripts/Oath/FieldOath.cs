4ada174f7f886bd7b1ad82d70700b7f2c0446972
keel-210 <crafters_touhou@yahoo.co.jp>
2020-04-20 04:01:55 +0900

    base ver0.6.1

---
+++ Assets/_Scripts/Oath/FieldOath.cs
@@ -0,0 +1,10 @@
+using System.Linq;
+using System.Collections.Generic;
+public class FieldOath : Oath
+{
+	public FieldOath(Board b, List<IPiece> l, bool IsWhite) : base(b, l, IsWhite) { }
+	public override void OathEffect()
+	{
+
+	}
+}
--- Assets/_Scripts/Pieces/Recstarionis.cs
+++ Assets/_Scripts/Pieces/Recstarionis.cs
@@ -9,14 +9,6 @@ public class Recstarionis : PieceBase
 	public override List<Vector2Int> CheckMovement()
 	{
 		List<Vector2Int> t = new List<Vector2Int>();
-		if (board.pieces[PositionOnBoard.x, PositionOnBoard.y + 1] == null)
-		{
-			t.Add(new Vector2Int(0, 1) + PositionOnBoard);
-			return t;
-		}
-
-		if (board.pieces[PositionOnBoard.x, PositionOnBoard.y + 1].Stage <= Stage)
-			t.Add(new Vector2Int(0, 1) + PositionOnBoard);
 		return t;
 	}
 }
