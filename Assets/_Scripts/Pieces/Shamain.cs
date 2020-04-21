a828cbd14ad0ad1870b3d0e0ae57b85f1f3b654c
keel-210 <crafters_touhou@yahoo.co.jp>
2020-04-21 23:22:56 +0900

    Rule ver0.5.1

--- Assets/_Scripts/Pieces/Shamain.cs
+++ Assets/_Scripts/Pieces/Shamain.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Shamain : PieceBase
 {
+	bool IsFirstMoved = false;
 	protected override void Initialize()
 	{
 		pieceType = PieceType.Shamain;
@@ -9,14 +10,39 @@ public class Shamain : PieceBase
 	public override List<Vector2Int> CheckMovement()
 	{
 		List<Vector2Int> t = new List<Vector2Int>();
-		if (board.pieces[PositionOnBoard.x, PositionOnBoard.y + 1] == null)
-		{
-			t.Add(new Vector2Int(0, 1) + PositionOnBoard);
-			return t;
-		}
-
-		if (board.pieces[PositionOnBoard.x, PositionOnBoard.y + 1].Stage <= Stage)
-			t.Add(new Vector2Int(0, 1) + PositionOnBoard);
+		ShamainCheck(t);
 		return t;
+	}
+	void ShamainCheck(List<Vector2Int> temp)
+	{
+		List<Vector2Int> ShamainMovement = new List<Vector2Int> { new Vector2Int(0, 1), new Vector2Int(1, 1), new Vector2Int(-1, 1) };
+		for (int i = 0; i < ShamainMovement.Count; i++)
+			if (!IsWhitePlayer)
+				ShamainMovement[i] = new Vector2Int(-1 * ShamainMovement[i].x, -1 * ShamainMovement[i].y);
+
+		Vector2Int Forward = PositionOnBoard + ShamainMovement[0];
+		Vector2Int RightForward = PositionOnBoard + ShamainMovement[1];
+		Vector2Int LeftForward = PositionOnBoard + ShamainMovement[2];
+
+		if (BoardSanityCheck(Forward) || BoardSanityCheck(RightForward) || BoardSanityCheck(LeftForward))
+			return;
+		IPiece f = board.pieces[Forward.x, Forward.y], r = board.pieces[RightForward.x, RightForward.y], l = board.pieces[LeftForward.x, LeftForward.y];
+		if (f == null)
+		{
+			temp.Add(Forward + PositionOnBoard);
+			if (!IsFirstMoved)
+			{
+				temp.Add(Forward + Forward + PositionOnBoard);
+				IsFirstMoved = true;
+			}
+		}
+		if (r != null && r.Stage <= Stage && (IsWhitePlayer ^ r.IsWhitePlayer))
+			temp.Add(RightForward + PositionOnBoard);
+		if (l != null && l.Stage <= Stage && (IsWhitePlayer ^ l.IsWhitePlayer))
+			temp.Add(LeftForward + PositionOnBoard);
+	}
+	bool BoardSanityCheck(Vector2Int v)
+	{
+		return v.x < 0 || board.size - 1 < v.x || v.y < 0 || board.size - 1 < v.y;
 	}
 }
